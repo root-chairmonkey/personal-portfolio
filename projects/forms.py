f32fd340d7d850553d9efb4ad498ffa77115ed4e
root-chairmonkey <root.lam28@gmail.com>
2021-11-17 13:21:20 +0530

    update to project_index.html

--- projects/forms.py
+++ projects/forms.py
@@ -1,8 +1,20 @@
 from django import forms
+from django.db.models.base import Model
+from django.forms import fields, widgets
 from django.utils import timezone
+from .models import Project
 
-class ProjectForm(forms.Form):
-    title = forms.CharField(max_length=100)
-    description = forms.CharField(widget=forms.Textarea)
-    technology = forms.ChoiceField(choices=(("tech", "tech"),))
-    collaborator = forms.CharField(max_length=100)
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ['title', 'description', 'technology', 'collaborator']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'id': 'titleid'}),
+            'description': forms.TextInput(attrs={'class': 'form-control', 'id': 'descriptionid'}),
+            'technology' : forms.TextInput(attrs={'class': 'form-control', 'id': 'technologyid'}),
+            'collaborator': forms.TextInput(attrs={'class': 'form-control', 'id': 'collaboratorid'}),                                  
+        }
+    #title = forms.CharField(max_length=100)
+    #description = forms.CharField(widget=forms.Textarea)
+    #technology = forms.ChoiceField(choices=(("tech", "tech"),))
+    #collaborator = forms.CharField(max_length=100)

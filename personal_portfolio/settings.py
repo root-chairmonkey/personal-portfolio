4502cc41fa689576ab4e402667ef31c32646ebe2
chair-monkey <root.lam28@gmail.com>
2021-12-02 12:43:19 +0530

    pycache changes

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os.path
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,4 +139,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'projects/static'), os.path.join('boot
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Activate Django Heroku
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
